b0781c8e4d9ad3ba13a583515b94721931fd1b3e
Chris Karr <chris@audacious-software.com>
2023-03-27 12:21:54 -0500

    Code restructuring

--- js/content.js
+++ js/content.js
@@ -1,17 +1,19 @@
 /* global chrome, locationFilterMatches */
 
-window.registerModuleCallback(function (config) {
-  // console.log('[Cookies] Checking host...')
-  // console.log(config)
+(function () {
+  window.registerModuleCallback(function (config) {
+    // console.log('[Cookies] Checking host...')
+    // console.log(config)
 
-  if (locationFilterMatches(window.location, config['cookie-filters'])) {
-    console.log('[Cookies] Recording cookies from ' + window.location + '...')
-    chrome.runtime.sendMessage({
-      content: 'record_cookies',
-      url: window.location.href,
-      pageTitle: document.title
-    })
-  } else {
-    console.log('[Cookies] Skipping ' + window.location + '.')
-  }
-})
+    if (locationFilterMatches(window.location, config['cookie-filters'])) {
+      console.log('[Cookies] Recording cookies from ' + window.location + '...')
+      chrome.runtime.sendMessage({
+        content: 'record_cookies',
+        url: window.location.href,
+        pageTitle: document.title
+      })
+    } else {
+      console.log('[Cookies] Skipping ' + window.location + '.')
+    }
+  })
+})(); // eslint-disable-line semi, no-trailing-spaces
