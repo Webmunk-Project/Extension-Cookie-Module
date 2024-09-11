d615ff5f7789b3b26c54a2fb9fa07cccf0218c39
Chris Karr <github@audacious-software.com>
2024-09-11 18:38:56 -0500

    Improved support for detecting top-level pages

--- js/content.js
+++ js/content.js
@@ -1,19 +1,29 @@
 /* global chrome, locationFilterMatches */
 
 (function () {
-  window.registerModuleCallback(function (config) {
-    // console.log('[Cookies] Checking host...')
-    // console.log(config)
+  console.log('[Cookie Logger] Code loaded...')
 
-    if (locationFilterMatches(window.location, config['cookie-filters'])) {
-      console.log('[Cookies] Recording cookies from ' + window.location + '...')
+  window.registerModuleCallback(function (config) {
+    if (config['cookie-filters'] === undefined || locationFilterMatches(window.location, config['cookie-filters'])) {
+      let isTopLevel = false
+      
+      try {
+        isTopLevel = window.location === window.parent.location
+      } catch (ex) {
+        // Security block - not at top level.
+        console.log(ex)
+      }
+
+      console.log(`[Cookie Logger] Recording cookies from ${window.location} (${isTopLevel})...`)
+
       chrome.runtime.sendMessage({
         content: 'record_cookies',
         url: window.location.href,
-        pageTitle: document.title
+        pageTitle: document.title,
+        topLevel: isTopLevel
       })
     } else {
-      console.log('[Cookies] Skipping ' + window.location + '.')
+      console.log(`[Cookie Logger] Skipping ${window.location}.`)
     }
   })
 })(); // eslint-disable-line semi, no-trailing-spaces
--- js/worker.js
+++ js/worker.js
@@ -1,45 +1,108 @@
-/* global chrome, handleMessage, registerCustomModule, registerMessageHandler */
+/* global chrome, handleMessage, registerCustomModule, registerMessageHandler, generateSecureHash */
 
-const recordCookies = function (request, sender, sendResponse) {
-  console.log('[Cookie] Recording cookies for ' + request.url + '...')
+(function () {
+  let cookieConfig = {}
 
-  if (request.content === 'record_cookies') {
-    const payload = {
-      'url*': request.url,
-      'page-title*': request.pageTitle,
-      cookies: []
+  const annotateCookie = function (cookie) {
+    cookie.category = 'Unknown'
+
+    const cookieName = cookie.name.toLowerCase()
+
+    let cookieDomain = null
+
+    if (cookieConfig.annotations !== undefined) {
+      if (cookieConfig.annotations[cookie.domain] !== undefined) {
+        cookieDomain = cookieConfig.annotations[cookie.domain]
+      } else if (cookieConfig.annotations['*'] !== undefined) {
+        cookieDomain = cookieConfig.annotations[cookie.domain]
+      }
     }
 
-    chrome.cookies.getAll({
-      url: request.url
-    },
-    function (cookies) {
-      cookies.forEach(function (cookie) {
-        console.log(cookie.name + ' --> ' + cookie.name)
-        console.log(cookie)
+    let cookieSettings = null
 
-        payload.cookies.push(cookie)
-      })
-
-      if (payload.cookies.length > 0) {
-        const newRequest = {
-          content: 'record_data_point',
-          generator: 'browser-cookies',
-          payload: payload // eslint-disable-line object-shorthand
-        }
-
-        handleMessage(newRequest, sender, sendResponse)
+    if (cookieDomain !== null) {
+      if (cookieDomain[cookieName] !== undefined) {
+        cookieSettings = cookieDomain[cookieName]
+      } else if (cookieDomain['*'] !== undefined) {
+        cookieDomain = cookieDomain['*']
       }
-    })
+    }
 
-    return true
+    if (cookieSettings === null || cookieSettings === undefined) {
+      // Default configuration
+
+      cookieSettings = {
+        category: 'Unknown',
+        'keep-value': false
+      }
+    }
+
+    if (cookieSettings.exclude) {
+      cookie.exclude = true
+    } else {
+      if (cookieSettings.category !== undefined) {
+        cookie.category = cookieSettings.category
+      }
+
+      if (cookieSettings['keep-value'] !== true) {
+        cookie.value = generateSecureHash(cookie.value)
+      }
+    }
   }
 
-  return false
-}
+  const recordCookies = function (request, sender, sendResponse) {
+    console.log('[Cookie Logger] Recording cookies for ' + request.url + '...')
 
-registerCustomModule(function (config) {
-  console.log('[Cookies] Initialized.')
+    if (request.content === 'record_cookies') {
+      const payload = {
+        'url*': request.url,
+        'page-title*': request.pageTitle,
+        'top-level': request.topLevel,
+        'root-url': sender.tab.url,
+        'root-title': sender.tab.title,
+        cookies: []
+      }
 
-  registerMessageHandler('record_cookies', recordCookies)
-})
+      chrome.cookies.getAll({
+        url: request.url
+      },
+      function (cookies) {
+        cookies.forEach(function (cookie) {
+          annotateCookie(cookie)
+
+          if (cookie.exclude !== true) {
+            payload.cookies.push(cookie)
+          }
+        })
+
+        if (payload.cookies.length > 0) {
+          const newRequest = {
+            content: 'record_data_point',
+            generator: 'browser-cookies',
+            payload: payload // eslint-disable-line object-shorthand
+          }
+
+          handleMessage(newRequest, sender, sendResponse)
+        }
+      })
+
+      return true
+    }
+
+    return false
+  }
+
+  registerCustomModule(function (config) {
+    cookieConfig = config.cookies
+
+    if (cookieConfig === undefined) {
+      cookieConfig = {}
+    }
+
+    if (cookieConfig.enabled) {
+      console.log('[Cookie Logger] Initialized.')
+
+      registerMessageHandler('record_cookies', recordCookies)
+    }
+  })
+})()
